ab15f1412e4b319ff6f97c9d7d6f9f6ecd83268d
Dave Keen <dev@ruffness.com>
2015-10-11 20:23:14 +0200

    Added 'back' as an option

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import React from 'react'
 const { bool, object, string, func } = React.PropTypes
 
 function isLeftClickEvent(event) {
-  return event.button === 0
+    return event.button === 0
 }
 
 function isModifiedEvent(event) {
-  return !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
+    return !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
 }
 
 function isEmptyObject(object) {
-  for (const p in object)
-    if (object.hasOwnProperty(p))
-      return false
+    for (const p in object)
+        if (object.hasOwnProperty(p))
+            return false
 
-  return true
+    return true
 }
 
 /**
@@ -46,86 +46,85 @@ function isEmptyObject(object) {
  * 
  * historyType
  * ===========
- * This can be set to either `push` or `replace`.  It defaults to 'push', and
- * selects when the Link uses `pushState` or `replaceState`.
+ * This can be set to either `push`, `replace` or `back`.
+ * 
+ * `push`: this adds the location to the history using `pushState`
+ * `replace`: this replaces the top of the history using `replaceState`
+ * `back`: this goes back to the previous history location in the stack.  If you use
+ * this option you shouldn't specify a `to` parameter.
  */
-const Link = React.createClass({
-
-  contextTypes: {
-    history: object
-  },
-
-  propTypes: {
-    activeStyle: object,
-    activeClassName: string,
-    onlyActiveOnIndex: bool.isRequired,
-    to: string.isRequired,
-    query: object,
-    state: object,
-    onClick: func,
-    eventName: string.isRequired,
-    historyType: string.isRequired
-  },
-
-  getDefaultProps() {
-    return {
-      onlyActiveOnIndex: false,
-      className: '',
-      style: {},
-      eventName: 'onClick',
-      historyType: 'push'
+class Link extends React.Component {
+    static contextTypes = {
+        history: object
     }
-  },
-
-  handleClick(event) {
-    let allowTransition = true, clickResult
-
-    if (this.props.onClick)
-      clickResult = this.props.onClick(event)
-
-    if (isModifiedEvent(event))
-      return
-
-    if (clickResult === false || event.defaultPrevented === true)
-      allowTransition = false
-
-    event.preventDefault()
-
-    if (allowTransition) {
-      if (this.props.historyType === 'push') {
-        this.context.history.pushState(this.props.state, this.props.to, this.props.query);
-      } else if (this.props.historyType === 'replace') {
-        this.context.history.replaceState(this.props.state, this.props.to, this.props.query);
-      } else {
-        console.error(`Only 'push' and 'replace' are supported as historyTypes`);
-      }
+    static propTypes = {
+        activeStyle: object,
+        activeClassName: string,
+        onlyActiveOnIndex: bool.isRequired,
+        to: string,
+        query: object,
+        state: object,
+        onClick: func,
+        eventName: string.isRequired,
+        historyType: string.isRequired
     }
-  },
+    static defaultProps = {
+        onlyActiveOnIndex: false,
+        className: '',
+        style: {},
+        eventName: 'onClick',
+        historyType: 'push'
+    }
 
-  render() {
-    const { history } = this.context
-    const { activeClassName, activeStyle, onlyActiveOnIndex, to, query, state, onClick, eventName, ...props } = this.props
+    handleClick(event) {
+        let allowTransition = true, clickResult
 
-    //props.onClick = this.handleClick
-    props[eventName] = this.handleClick
+        if (this.props.onClick)
+            clickResult = this.props.onClick(event)
 
-    // Ignore if rendered outside the context
-    // of history, simplifies unit testing.
-    if (history) {
-      if (activeClassName || (activeStyle != null && !isEmptyObject(activeStyle))) {
-        if (history.isActive(to, query, onlyActiveOnIndex)) {
-          if (activeClassName)
-            props.className += props.className === '' ? activeClassName : ` ${activeClassName}`
+        if (isModifiedEvent(event))
+            return
 
-          if (activeStyle)
-            props.style = { ...props.style, ...activeStyle }
+        if (clickResult === false || event.defaultPrevented === true)
+            allowTransition = false
+
+        event.preventDefault()
+
+        if (allowTransition) {
+            if (this.props.historyType === 'push') {
+                this.context.history.pushState(this.props.state, this.props.to, this.props.query);
+            } else if (this.props.historyType === 'replace') {
+                this.context.history.replaceState(this.props.state, this.props.to, this.props.query);
+            } else if (this.props.historyType === 'back') {
+                this.context.history.goBack();
+            } else {
+                console.error(`Only 'push', 'replace' and 'back' are supported as historyTypes`);
+            }
         }
-      }
     }
 
-    return React.createElement('a', props)
-  }
+    render() {
+        const { history } = this.context
+        const { activeClassName, activeStyle, onlyActiveOnIndex, to, query, state, onClick, eventName, ...props } = this.props
 
-})
+        props[eventName] = this.handleClick.bind(this);
+
+        // Ignore if rendered outside the context
+        // of history, simplifies unit testing.
+        if (history) {
+            if (activeClassName || (activeStyle != null && !isEmptyObject(activeStyle))) {
+                if (history.isActive(to, query, onlyActiveOnIndex)) {
+                    if (activeClassName)
+                        props.className += props.className === '' ? activeClassName : ` ${activeClassName}`
+
+                    if (activeStyle)
+                        props.style = { ...props.style, ...activeStyle }
+                }
+            }
+        }
+
+        return React.createElement('a', props)
+    }
+}
 
 export { Link }
